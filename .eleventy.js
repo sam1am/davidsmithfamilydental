94d722e76c13d49d7b23bbdc9d0886290d6884cb
SamG <samgarfield@gmail.com>
2025-01-10 18:02:50 -0700

    🚀feat(eleventy) add groupBy filter for array transformation 🛠️refactor(nav) update services link path 🚀feat(services) create a new services page with grouped listings

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,16 @@ module.exports = function (eleventyConfig) {
     // Copy assets directory
     eleventyConfig.addPassthroughCopy("src/assets");
 
+    // Add groupBy filter
+    eleventyConfig.addFilter("groupBy", function(array, key) {
+        return array.reduce((groups, item) => {
+            const group = (groups[item[key]] || []);
+            group.push(item);
+            groups[item[key]] = group;
+            return groups;
+        }, {});
+    });
+
     return {
         dir: {
             input: "src",
